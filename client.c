36f2c3cd7a600c903ef799f34a244949e53c9cbe
mehmet <mehmet@mehmet-Satellite-P855.(none)>
2013-02-11 15:45:09 -0500

    c_s working finally

--- client.c
+++ client.c
@@ -14,9 +14,7 @@
 
 #include <arpa/inet.h>
 
-#define PORT "3490" // the port client will be connecting to 
-
-#define MAXDATASIZE 100 // max number of bytes we can get at once 
+#define PORT "4000" // the port client will be connecting to 
 
 // get sockaddr, IPv4 or IPv6:
 void *get_in_addr(struct sockaddr *sa)
@@ -28,16 +26,30 @@ void *get_in_addr(struct sockaddr *sa)
     return &(((struct sockaddr_in6*)sa)->sin6_addr);
 }
 
-int main(int argc, char *argv[])
+#define MAXSENDLENGTH 100
+#define MAXRECVLENGTH 100
+int nofChunksToSend=0;
+int sockfd;
+FILE *file; //For debugging purposes
+//Some buffers necessary for communication
+char send_buf[MAXSENDLENGTH];
+char recv_buf[MAXSENDLENGTH];
+
+//Method pre_decs
+int sendTo(int socketFD, char* send_buf);
+int recvFrom(int socketFD);
+void close_();
+int
+main(int argc, char *argv[])
 {
-    int sockfd, numbytes;  
-    char buf[MAXDATASIZE];
+//	int sockfd;
+    int numbytes;  
     struct addrinfo hints, *servinfo, *p;
     int rv;
     char s[INET6_ADDRSTRLEN];
 
-    if (argc != 2) {
-        fprintf(stderr,"usage: client hostname\n");
+    if (argc != 3) {
+        fprintf(stderr,"usage: client hostname, # of chunks to be received\n");
         exit(1);
     }
 
@@ -77,36 +89,58 @@ int main(int argc, char *argv[])
     printf("client: connecting to %s\n", s);
 
     freeaddrinfo(servinfo); // all done with this structure
-	//Receive the bulk data from the server
-	FILE *file; 
+	
+	//Communicate with the server
+	recvFrom(sockfd); //recv the inquiry of server
+	if(strcmp(recv_buf, "WDW?")==0){
+		printf("WDW? received\n");
+		sendTo(sockfd, argv[2]); //the # of chunks that client desires to get
+	}
+
 	file = fopen("file.txt", "w");
 	fprintf(file,"%s","Top of the file");
-	//Simply try to receive multiple messages
-	if ((numbytes = recv(sockfd, buf, MAXDATASIZE-1, 0)) == -1) {
-	    perror("recv");
-	}
-	buf[numbytes] = '\0';
-	printf("client: received '%s'\n",buf);
-	memset(buf, 0, numbytes);
-	if ((numbytes = recv(sockfd, buf, MAXDATASIZE-1, 0)) == -1) {
-	    perror("recv");
-	}
-	buf[numbytes] = '\0';
-	printf("client: received '%s'\n",buf);
-	memset(buf, 0, numbytes);
-/*
+	//Receive the bulk data from the server
 	while(1){
-		if ((numbytes = recv(sockfd, buf, MAXDATASIZE-1, 0)) == -1) {
-		    perror("recv");
-		    exit(1);
-		}
-		buf[numbytes] = '\0';
-		printf("client: received '%s'\n",buf);
-		fprintf(file,"%s",buf);
+		numbytes=recvFrom(sockfd);
+		printf("numbytes recved: %d\n",numbytes);
+		printf("CLIENT RECEIVED: '%s'\n",recv_buf);
+		fprintf(file,"\n%s",recv_buf);
+		memset(recv_buf, 0, MAXRECVLENGTH);
 	}
-*/
+	close_();
     close(sockfd); //Socket will be closed by the server when all of the data is sent
-	fclose(file); /*done!*/ 
-
+	fclose(file);
     return 0;
 }
+
+void close_()
+{
+	close(sockfd); //Socket will be closed by the server when all of the data is sent
+	fclose(file);
+}
+
+int sendTo(int socketFD, char* send_buf){
+	if (send(socketFD, send_buf, (int)strlen(send_buf), 0) != -1){
+		return 1; //sent successfully
+	}
+	else{
+        perror("send");
+		return -1; //could not be sent
+	}
+}
+//
+int recvFrom(int socketFD){
+	int numbytes = recv(socketFD, recv_buf, MAXRECVLENGTH, 0); //No flags are necessary
+	if(numbytes>0){//Successful reception
+		return numbytes;
+	}	
+	else if (numbytes == 0) {//socket is closed by the other side
+		close_();
+	    exit(1);
+	}
+	else{ //Unsuccessful reception
+		perror("recv");
+		return -1;
+	}
+}
+
--- server.c
+++ server.c
@@ -15,10 +15,8 @@
 #include <sys/wait.h>
 #include <signal.h>
 
-#define PORT "3490"  // the port users will be connecting to
-
+#define PORT "4000"  // the port users will be connecting to
 #define BACKLOG 10     // how many pending connections queue will hold
-#define nofChunkToSend 100
 
 void sigchld_handler(int s)
 {
@@ -35,7 +33,18 @@ void *get_in_addr(struct sockaddr *sa)
     return &(((struct sockaddr_in6*)sa)->sin6_addr);
 }
 
-int main(void)
+#define MAXSENDLENGTH 100
+#define MAXRECVLENGTH 100
+int nofChunksToSend=0;
+//Some buffers necessary for communication
+char send_buf[MAXSENDLENGTH];
+char recv_buf[MAXRECVLENGTH];
+char temp_buffer[10];
+
+int sendTo(int socketFD, char* send_buf);
+int recvFrom(int socketFD);
+int
+main(void)
 {
     int sockfd, new_fd;  // listen on sock_fd, new connection on new_fd
     struct addrinfo hints, *servinfo, *p;
@@ -46,7 +55,7 @@ int main(void)
     char s[INET6_ADDRSTRLEN];
     int rv;
 	pid_t childPID; //control variable for multithreading
-
+	//char send_buf[MAX_SEND_BUF_LENGTH];
     memset(&hints, 0, sizeof hints);
     hints.ai_family = AF_UNSPEC;
     hints.ai_socktype = SOCK_STREAM;
@@ -127,16 +136,26 @@ int main(void)
 		{
 			if(childPID == 0){ //Child process
 				close(sockfd); // child doesn't need the listener
-				//Simple trial to send multiple messages
-				if (send(new_fd, "Hello, world!", 13, 0) != -1){
-					perror("send");
-				}
-				if (send(new_fd, "Hello, world!", 13, 0) != -1){
-					perror("send");
-				}
+				//Send "WDW?"; What do you want?
+				sendTo(new_fd, "WDW?");
+				recvFrom(new_fd);
+				nofChunksToSend=atoi(recv_buf);
+				//send the bulk data to the client
+				unsigned int nof_chunksent=0;
+				while(nof_chunksent < nofChunksToSend){
+					strcpy(send_buf, "Hello World birader");
+					sprintf(temp_buffer, "%d", nof_chunksent);
+					strcat(send_buf, temp_buffer);
+					//printf("send_buf: %s\n", send_buf);
+					//send send_buf to receiver
+					if (sendTo(new_fd, send_buf) == 1){
+						++nof_chunksent;
+					}
+					memset(send_buf, 0, MAXSENDLENGTH);//Initialize for the next chunk sending session
+					memset(temp_buffer, 0, 10);
+				}//All bulk data sent, close the socket
 			    close(new_fd);
 				fclose(file);
-				//break; //To make the child process exit from inf while
 				exit(0);
 			}
 			else{//parent process
@@ -148,32 +167,34 @@ int main(void)
 			printf("\n Fork failed, exit !\n");
 	        return 1;
 		}
-/*
-        if (!fork()) { // this is the child process
-            close(sockfd); // child doesn't need the listener
-			//Simple trial to send multiple messages
-			if (send(new_fd, "Hello, world!", 13, 0) != -1){
-				perror("send");
-			}
-			if (send(new_fd, "Hello, world!", 13, 0) != -1){
-				perror("send");
-			}
-	        close(new_fd);	
-*/
-/*
-		    //send the bulk data to the client
-			unsigned char nof_chunksent=0;
-			while(nof_chunksent < nofChunkToSend){
-				if (send(new_fd, "Hello, world!", 13, 0) != -1){
-					++nof_chunksent;
-				}
-				else
-	                perror("send");
-			}//All bulk data sent, close the socket
-			close(new_fd);
-*/
+
 	}
 	fclose(file); /*done!*/ 
 
     return 0;
 }
+int sendTo(int socketFD, char* send_buf){
+	if (send(socketFD, send_buf, (int)strlen(send_buf), 0) != -1){
+		return 1; //sent successfully
+	}
+	else{
+        perror("send");
+		return -1; //could not be sent
+	}
+}
+//
+int recvFrom(int socketFD){
+	int numbytes = recv(socketFD, recv_buf, MAXRECVLENGTH, 0); //No flags are necessary
+	printf("Server recved: %s", recv_buf);
+	if(numbytes>0){//Successful reception
+		return 1;
+	}	
+	else if (numbytes == 0) {//socket is closed by the other side
+	    exit(1);
+	}
+	else{ //Unsuccessful reception
+		perror("recv");
+		return -1;
+	}
+}
+
